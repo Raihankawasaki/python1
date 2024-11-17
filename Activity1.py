f05c2fd67e8fc6e3d6e9f5dbc8d4f81c6c90c42f
Raihankawasaki <raihansmohammed12345@gmail.com>
2024-11-17 21:12:17 +0400

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,47 @@
+#Write a program for print command.
+import keyword
+
+print(keyword.kwlist)
+print("Raihan")
+print(1)
+print("Welcome to the world of python!")
+print("python is one of many text-based coding languages.")
+print(987654321)
+#Write a program to create different variables to store various values.
+
+name="Raihan Shiju Mohammad"
+print(name)#variable are not written in quotation
+
+age=10
+print(age)
+
+dob="12-nov-2017"
+print(dob)
+
+
+
+place="Abu dhabi"
+print(place)
+number=1234567889
+print(number)
+road="raihan_8970"
+print(road)
+
+country=input("Enter the name of your country \n")#\n is used for bringing u to the next line
+print(country)
+date_of_birth=input("enter your date of birth. \n")
+print(date_of_birth)
+
+print("I HAVE",5 ,"CARS",4,"bikes")
+
+num=input("Enter any digit:")
+print("The number entered by the user is : ", num)
+
+hobbie=input("what is your hobbie?")
+print=("hobbie=",hobbie)
+
+
+
+
+
+#Write a program to print all the keywords
